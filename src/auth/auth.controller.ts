777222b75aca1e5be8cbe4443fc0a708478ff4c7
JudeAshin <judeashin258@gmail.com>
2023-07-14 17:48:40 +0530

    used jwd guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthenticateDto } from './dto/authenticate.dto';
+import { JwtAuthGuard } from 'src/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    profile(@Req() req,@Res() res){
+    return res.status(HttpStatus.OK).json(req.user);
+}
     @Post()
 
     login(@Res() res, @Body() AuthenticateDto: AuthenticateDto) {
